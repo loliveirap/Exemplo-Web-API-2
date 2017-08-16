18efebcbdb306ec519c237042dbbaea28ffb9848
loliveirap <lep_o@hotmail.com>
2017-08-16 19:51:54 -0300

    Add files via upload

---
+++ DevStore.Api/Controllers/ProductController.cs
@@ -0,0 +1,140 @@
+﻿using DevStore.Data.DataContexts;
+using DevStore.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace DevStore.Api.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    [RoutePrefix("api/v1/public")]
+    public class ProductController : ApiController
+    {
+        private DevStoreDataContext db = new DevStoreDataContext();
+
+        //GET
+        [Route("product")]
+        public IQueryable<Product> GetProduct()
+        {
+            return db.Products;
+        }
+
+        //GET
+        [Route("products")]
+        public HttpResponseMessage GetProducts()
+        {
+            var result = db.Products.Include("Category").ToList();
+            return Request.CreateResponse(HttpStatusCode.OK, result);
+        }
+
+        //GET
+        [Route("categories")]
+        public HttpResponseMessage GetCategories()
+        {
+            var result = db.Categories.ToList();
+            return Request.CreateResponse(HttpStatusCode.OK, result);
+        }
+
+        //GET
+        [Route("categories/{categoryId}/products")]
+        public HttpResponseMessage GetProductsByCategories(int categoryId)
+        {
+            var result = db.Products.Include("Category").Where(x => x.CategoryId == categoryId).ToList();
+            return Request.CreateResponse(HttpStatusCode.OK, result);
+        }
+
+        //POST
+        [HttpPost]
+        [Route("products")]
+        public HttpResponseMessage PostProduct(Product product)
+        {
+            if (product == null)
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+
+            try
+            {
+                db.Products.Add(product);
+                db.SaveChanges();
+
+                var result = product;
+                return Request.CreateResponse(HttpStatusCode.OK, result);
+            }
+            catch (Exception)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, "Falha ao incluir produto");
+            }
+        }
+
+        // alterar a classe quando for modificada
+        [HttpPatch]
+        [Route("products")]
+        public HttpResponseMessage PatchProduct(Product product)
+        {
+            if (product == null)
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+
+            try
+            {
+                db.Entry<Product>(product).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+
+                var result = product;
+                return Request.CreateResponse(HttpStatusCode.OK, result);
+            }
+            catch (Exception)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, "Falha ao alterar o produto");
+            }
+        }
+
+        [HttpPut]
+        [Route("products")]
+        public HttpResponseMessage PutProduct(Product product)
+        {
+            if (product == null)
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+
+            try
+            {
+                db.Entry<Product>(product).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+
+                var result = product;
+                return Request.CreateResponse(HttpStatusCode.OK, result);
+            }
+            catch (Exception)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, "Falha ao alterar o produto");
+            }
+        }
+
+        [HttpDelete]
+        [Route("products")]
+        public HttpResponseMessage DeleteProduct(int productId)
+        {
+            if (productId <= 0)
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+
+            try
+            {
+                db.Products.Remove(db.Products.Find(productId));
+                db.SaveChanges();
+
+                return Request.CreateResponse(HttpStatusCode.OK, "Produto excluído");
+            }
+            catch (Exception)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, "Falha ao excluir o produto");
+            }
+        }
+        
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+        }
+    }
+}
